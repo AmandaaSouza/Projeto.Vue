7a0691b589cb8d42b9a152e6b41b1e8e69e6a65d
AmandaaSouza <161317868+AmandaaSouza@users.noreply.github.com>
2024-08-02 11:49:03 -0300

    Add files via upload

---
+++ MenuPrincipal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  };
+  </script>
+  
+  <style>
+  /* estilos do seu app */
+  </style>
+  /** */
+  <template>
+    <nav>
+      <router-link to="/videos">Vídeos Diversos</router-link>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavigationComponent'
+  };
+  </script>
+  
+  <style>
+  /* estilos do componente de navegação */
+  </style>
